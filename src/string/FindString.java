d1acaacc59a57b289030f412ae90b68f5103bc5a
hanna1122 <khn951410@naver.com>
2024-03-27 14:14:15 +0900

    feat: change package name

--- src/string/FindString.java
+++ src/string/FindString.java
@@ -1,8 +1,8 @@
-package String;
+package string;
 
 import java.util.Scanner;
 
-public class findString {
+public class FindString {
     public int solution(String str, char t) {
         int answer = 0;
         str = str.toUpperCase();
@@ -15,7 +15,7 @@ public class findString {
     }
 
     public static void main(String[] args) {
-        findString t = new findString();
+        FindString t = new FindString();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
         char c= scanner.next().charAt(0);
